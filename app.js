e98a979af3ab352abfa4b46eadd5214d3538b6ff
UNIZA\uniza.lin <unizalin@gmail.com>
2019-04-10 16:11:51 +0800

    add route

--- app.js
+++ app.js
@@ -24,6 +24,45 @@ db.once('open', () => {
 app.get('/', (req, res) => {
   res.send('hello word')
 });
+// Todo 首頁
+app.get('/', (req, res) => {
+  res.send('hello world!')
+})
+
+// 列出全部 Todo
+app.get('/todos', (req, res) => {
+  res.send('列出所有 Todo')
+})
+
+// 新增一筆 Todo 頁面
+app.get('/todos/new', (req, res) => {
+  res.send('新增 Todo 頁面')
+})
+
+// 顯示一筆 Todo 的詳細內容
+app.get('/todos/:id', (req, res) => {
+  res.send('顯示 Todo 的詳細內容')
+})
+
+// 新增一筆  Todo
+app.post('/todos', (req, res) => {
+  res.send('建立 Todo')
+})
+
+// 修改 Todo 頁面
+app.get('/todos/:id/edit', (req, res) => {
+  res.send('修改 Todo 頁面')
+})
+
+// 修改 Todo
+app.post('/todos/:id', (req, res) => {
+  res.send('修改 Todo')
+})
+
+// 刪除 Todo
+app.post('/todos/:id/delete', (req, res) => {
+  res.send('刪除 Todo')
+})
 
 app.listen(3000, () => {
   console.log('app 3000 port is ready');
