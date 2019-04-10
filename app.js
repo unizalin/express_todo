0bf8cbec00d52b5dfa9dc0a3724b2467875fad1a
UNIZA\uniza.lin <unizalin@gmail.com>
2019-04-10 14:42:53 +0800

    新增 Mongoose u3ru6

--- app.js
+++ app.js
@@ -1,5 +1,25 @@
 const express = require('express');
 const app = express();
+//載入 mongoose
+const mongoose = require('mongoose');
+//載入 todo model
+const Todo = require('./models/todo');
+
+//設定連線到 mongoDB
+mongoose.connect('mongodb://localhost/todo', { useNewUrlParser: true });
+
+// mongoose 連線後透過 mongoose.connection 拿到 Connection 的物件
+const db = mongoose.connection;
+
+//連線異常
+db.on('error', () => {
+  console.log('mongodb error');
+});
+//連線成功
+db.once('open', () => {
+  console.log('mongodb connect');
+});
+
 
 app.get('/', (req, res) => {
   res.send('hello word')
