34726233ec6fb03182b40b34354a88115b2da2f8
UNIZA\uniza.lin <unizalin@gmail.com>
2019-04-15 15:41:33 +0800

    Add sort feature (ascending) on index page

--- app.js
+++ app.js
@@ -37,10 +37,12 @@ db.once('open', () => {
 
 // Todo 首頁
 app.get('/', (req, res) => {
-  Todo.find((err, todos) => {                                 // 把 Todo model 所有的資料都抓回來
-    if (err) return console.error(err)
-    return res.render('index', { todos: todos })  // 將資料傳給 index 樣板
-  });
+  Todo.find({})
+    .sort({ name: 'asc' })
+    .exec((err, todos) => {
+      if (err) return console.error(err);
+      return res.render('index', { todos: todos })
+    });
 });
 
 
