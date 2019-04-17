5c6cd37175c1536ec3db741ce433aad8cddebd3d
UNIZA\uniza.lin <unizalin@gmail.com>
2019-04-17 17:46:14 +0800

    Add user router

---
+++ routes/user.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const router = express.Router();
+
+//登入頁面
+router.get('/login', (req, res) => {
+  res.render('login');
+});
+
+// 登入檢查
+router.post('login', (req, res) => {
+  res.render('login');
+});
+
+//註冊頁面
+router.get('register', (req, res) => {
+  res.render('register');
+});
+
+//註冊檢查
+router.post('register', (req, res) => {
+  res.render('register');
+});
+
+//登出
+router.post('logout', (req, res) => {
+  res.render('logout');
+});
+
+
+
+
+
+
+
+
+
+module.exports = router;
